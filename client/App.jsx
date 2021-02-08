d340b62ba1657fd51c033be3346f403f99eb5996
Zachary McCain <zacmmcca@gmail.com>
2021-02-07 20:34:11 -0600

    Implemented styles

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header.jsx';
 import About from './components/About.jsx';
 import Projects from './components/Projects.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
+import { Transition } from 'react-transition-group';
+
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0 },
+  exited:  { opacity: 0 },
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -29,12 +45,24 @@ class App extends React.Component {
     let about = this.state.about ? <About /> : null;
     let contact = this.state.contact ? <Contact /> : null;
     let projects = this.state.projects ? <Projects /> : null;
+    // const [aboutIn, setAboutIn] = useState(false);
     return (
       <div>
         <Header/>
         <div className="collapseContainer">
           <div name="about" onClick={this.handleClick}>About</div>
-          {about}
+            {/* <Transition in={this.state.about} timeout={duration}>
+              {state => (
+                <div style={{
+                  ...defaultStyle,
+                  ...transitionStyles[state]
+                }}>
+                  I'm a fade Transition!
+                </div>
+              )}
+            </Transition> */}
+              {about}
+
           <div name="projects" onClick={this.handleClick}>Projects</div>
           {projects}
           <span name="contact" onClick={this.handleClick}>Contact</span>
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <div className="header">
       <span className='center'>Zach McCain, Full-Stack Engineer</span>
-      <span className='center'><a href="https://github.com/zachMcCain"><img className='gitHub' src='/Users/ZachMcCain/Code/Projects/portfolio/dist/assets/25231.png'/></a></span>
+      <span className='center'><a href="https://github.com/zachMcCain"><img className='gitHub' src='/assets/25231.png'/></a></span>
     </div>
   )
 }
