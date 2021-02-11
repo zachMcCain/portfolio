1ec7c2559708db050e00ed018a132f0ca9026d44
Zachary McCain <zacmmcca@gmail.com>
2021-02-10 21:02:33 -0600

    Updated styles.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Header = (props) => {
   return (
-    <div className="header">
-      <span className='center'>Zach McCain, Full-Stack Engineer</span>
-      <span className='center'><a href="https://github.com/zachMcCain"><img className='gitHub' src='/assets/25231.png'/></a></span>
-    </div>
+    // <nav className="navbar navbar-default">
+      <div className="header">
+      <div className="container-fluid">
+        {/* <div className="navbar-header"> */}
+        {/* <a class="navbar-brand" href="#">Zach McCain, Full-Stack Engineer</a> */}
+          <span className='center'>Zach McCain, Full-Stack Engineer</span>
+        {/* </div> */}
+          <span className='center'><a href="https://github.com/zachMcCain"><img className='gitHub' src='/assets/25231.png'/></a></span>
+      </div>
+      </div>
+    // </nav>
   )
 }
 
